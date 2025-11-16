7e976d62a8f01a0a18d96474bd5ca7aada5f3fc6
Aleksey Taganov <aleksejzendos@gmail.com>
2025-11-17 00:10:11 +0300

    Add course enrollment view and enroll button

--- courses/urls.py
+++ courses/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.course_list, name='course_list'),
     path('<int:pk>/', views.course_detail, name='course_detail'),
     path('test-celery/', views.trigger_test_task, name='test_celery'),
+    path("<int:pk>/enroll/", views.enroll_in_course, name="enroll"),
+
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Course
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required   
+from .models import Course, Enrollment
 from django.http import HttpResponse
-from .tasks import test_celery_task
+from .tasks import test_celery_task, send_enrollment_notification
 
 
 
@@ -13,12 +14,34 @@ def course_list(request):
 def course_detail(request, pk):
     course = get_object_or_404(Course, pk=pk)
     lessons = course.lessons.all()
+
+    is_enrolled = False
+    if request.user.is_authenticated:
+        is_enrolled = Enrollment.objects.filter(user=request.user, course=course).exists()
     return render(request, 'courses/course_detail.html', {
         'course': course,
         'lessons': lessons,
+        'is_enrolled': is_enrolled
     })
 
 def trigger_test_task(request):
     # Отправляем задачу в Celery
     test_celery_task.delay()
     return HttpResponse("Задача Celery отправлена в очередь")
+
+@login_required
+def enroll_in_course(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+
+    enrollment, created = Enrollment.objects.get_or_create(
+        user=request.user,
+        course=course,
+    )
+
+    if created:
+        # Если запись на курс создана впервые — запускаем Celery-задачу
+        send_enrollment_notification.delay(request.user.id, course.id)
+
+    # Всегда возвращаем пользователя на страницу курса
+    return redirect('courses:course_detail', pk=course.id)
+
